b0b73c4c1a29b80399f6a8b50ff627c83143950c
Roman Eremeev <nightspotlight@users.noreply.github.com>
2020-09-13 19:19:34 +0200

    Upgrade to Terraform 0.13

--- config.tf
+++ config.tf
@@ -1,15 +1,21 @@
 terraform {
-  required_version = "~> 0.12"
+  required_version = "~> 0.13"
   required_providers {
-    cloudflare = "~> 2.1"
-    hcloud = "~> 1.12"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.1"
+    }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.12"
+    }
   }
 }
 
 provider "cloudflare" {
-  email = var.cloudflare_email
+  email      = var.cloudflare_email
   account_id = var.cloudflare_account_id
-  api_key = var.cloudflare_api_key
+  api_key    = var.cloudflare_api_key
 }
 
 provider "hcloud" {
