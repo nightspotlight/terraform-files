63fbee57f38478546cb3186a804357035e4d2cd3
Roman Eremeev <nightspotlight@users.noreply.github.com>
2021-09-28 23:52:02 +0200

    Turn on deletion protection

--- hcloud.tf
+++ hcloud.tf
@@ -20,10 +20,13 @@ resource "hcloud_server" "nextcloud" {
   server_type  = "cx21-ceph"
   location     = "nbg1"
   image        = "debian-10"
-  keep_disk    = true
   ssh_keys     = [hcloud_ssh_key.roman.id]
   user_data    = data.cloudinit_config.user-data.rendered
   firewall_ids = [hcloud_firewall.nextcloud.id]
+
+  keep_disk          = true
+  delete_protection  = true
+  rebuild_protection = true
 
   labels = merge(
     {
@@ -48,6 +51,8 @@ resource "hcloud_volume" "nextcloud-data" {
   server_id = hcloud_server.nextcloud.id
   automount = true
   format    = "xfs"
+
+  delete_protection = true
 
   labels = local.tags
 }
