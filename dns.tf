c88f2269c063b581b15ed64bff774d62370b015e
Roman Eremeev <nightspotlight@users.noreply.github.com>
2024-12-01 18:08:26 +0100

    Move DNS records into variables

--- dns.tf
+++ dns.tf
@@ -3,11 +3,9 @@ module "dns" {
 
   zone_id = cloudflare_zone.nightspotlight_me.id
 
-  a_records    = local.nextcloud_a_record
-  aaaa_records = local.nextcloud_aaaa_record
-  txt_records = {
-    "nightspotlight.me" = "v=spf1 -all",
-    "*._domainkey"      = "v=DKIM1; p=",
-    "_dmarc"            = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
-  }
+  a_records     = local.nextcloud_a_record
+  aaaa_records  = local.nextcloud_aaaa_record
+  cname_records = var.cname_records
+  mx_records    = var.mx_records
+  txt_records   = var.txt_records
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "cname_records" {
+  type    = map(object({ address = string, proxied = bool }))
+  default = {}
+}
+
+variable "mx_records" {
+  type    = map(object({ address = string, priority = number }))
+  default = {}
+}
+
+variable "txt_records" {
+  type    = map(string)
+  default = {}
+}
+
 # Common resource tags
 variable "additional_tags" {
   type    = map(string)
